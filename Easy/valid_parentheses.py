c5724ed46973d7757e1d4dd8dbda04b5db9d0887
Andrew Wu <andrew.m.wu@gmail.com>
2024-01-08 03:57:26 -0600

    First day of grind75!

---
+++ Easy/valid_parentheses.py
@@ -0,0 +1,53 @@
+'''
+Valid Parentheses
+
+Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+
+An input string is valid if:
+
+    Open brackets must be closed by the same type of brackets.
+    Open brackets must be closed in the correct order.
+    Every close bracket has a corresponding open bracket of the same type.
+
+ 
+
+Example 1:
+
+Input: s = "()"
+Output: true
+
+Example 2:
+
+Input: s = "()[]{}"
+Output: true
+
+Example 3:
+
+Input: s = "(]"
+Output: false
+
+ 
+
+Constraints:
+
+    1 <= s.length <= 104
+    s consists of parentheses only '()[]{}'.
+
+'''
+class Solution:
+    def isValid(self, s: str) -> bool:
+        closeds = []
+        open_to_closed = {
+            "(" : ")",
+            "[" : "]",
+            "{" : "}",
+        }
+        for c in s:
+            if c in open_to_closed:
+                closeds.append(open_to_closed[c])
+            else:
+                if len(closeds) == 0:
+                    return False
+                if not c == closeds.pop():
+                    return False
+        return len(closeds) == 0
